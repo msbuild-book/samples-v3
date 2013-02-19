eeb70737e9c735672c82ede1fc42276e2f62dc57
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-02-18 18:49:23 -0800

    Converting to use plain ADO.NET

--- ch03/ContactsSample/Controllers/HomeController.cs
+++ ch03/ContactsSample/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿namespace ContactsSample.Controllers
 {
+    using ContactsSample.Models;
     using System;
     using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data.SqlClient;
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
@@ -13,26 +16,59 @@
 
         public ActionResult Index()
         {
-            List<Contact> contacts = null;
-            using (var ctx = new ContactsSampleDbEntities()) {
-                contacts = (from c in ctx.Contacts
-                            select c).ToList();
-            }
-            return View(contacts);
+            return View(GetContacts());
         }
 
         public ActionResult Insert(Contact contact) {
             string name = "foo";
+            string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string insertStmt = @"Insert into Contact(FirstName,LastName) Values(@FirstName,@LastName)";
 
             if (contact != null) {
-                using (var ctx = new ContactsSampleDbEntities()) {
-                    ctx.Contacts.Add(contact);
-                    ctx.SaveChanges();
+                using (var connection = new SqlConnection(conString))
+                using (var cmd = new SqlCommand(insertStmt, connection)) {
+                    connection.Open();
+
+                    cmd.Parameters.Add("@FirstName", contact.FirstName);
+                    cmd.Parameters.Add("@LastName", contact.LastName);
+
+                    int result = cmd.ExecuteNonQuery();
                 }
             }
 
             return RedirectToAction("Index");
         }
 
+        private List<Contact> GetContacts() {
+            List<Contact> contacts = new List<Contact>();
+
+            string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string query = @"select id,FirstName,LastName from Contact";
+
+            using (var connection = new SqlConnection(conString)) 
+            using(var cmd = new SqlCommand(query,connection)){
+                connection.Open();
+                
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read()) {
+                    Contact contact = new Contact();
+                    contact.Id = (int)reader["Id"];
+                    
+                    if(reader["FirstName"] != null)
+                        contact.FirstName = (string)reader["FirstName"];
+
+                    if (reader["LastName"] != null)
+                        contact.LastName = (string)reader["LastName"];
+
+                    contacts.Add(contact);
+                }
+
+                
+            }
+
+            return contacts;
+        }
     }
 }
---
+++ ch03/ContactsSample/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsSample.Models {
+    public class Contact {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
